3d13c92925a0677442b42a85603a4c90db5e2c1a
Bzzmn <alvaro.acevedo.ing@gmail.com>
2025-06-02 18:07:28 -0400

    config file ok

--- src/config.tsx
+++ src/config.tsx
@@ -5,7 +5,7 @@ export const chatConfig: ChatWidgetConfig = {
     // langGraphApiUrl: "https://xcala-agent-main-234b5df414785657a4b5a013d240ab07.us.langgraph.app",
     assistantId: "app",
     langSmithApiKey: "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", 
-    audioApiUrl: "https://xcala-api.thefullstack.digital",
+    audioApiUrl: "https://clf45vyjka.execute-api.us-east-1.amazonaws.com/dev/GroqAudioTranscription",
     // audioApiUrl: "https://psct29qw0l.execute-api.us-east-1.amazonaws.com/dev",
 };
 
