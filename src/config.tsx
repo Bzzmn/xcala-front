f1aa7f0b451c308407eb8ac719176c3b326259a8
Bzzmn <alvaro.acevedo.ing@gmail.com>
2025-07-04 11:21:04 -0400

    feat: Wrap application in BrowserRouter to fix routing issues

--- src/config.tsx
+++ src/config.tsx
@@ -1,11 +1,11 @@
 import type { ChatWidgetConfig } from "xcala-chat-widget";
 
 export const chatConfig: ChatWidgetConfig = {
-    langGraphApiUrl: "https://64u8qqfffh.execute-api.us-east-1.amazonaws.com/dev",
+    langGraphApiUrl: import.meta.env.VITE_LANGGRAPH_API_URL,
     // langGraphApiUrl: "https://xcala-agent-main-234b5df414785657a4b5a013d240ab07.us.langgraph.app",
     assistantId: "app",
-    langSmithApiKey: "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", 
-    audioApiUrl: "https://clf45vyjka.execute-api.us-east-1.amazonaws.com/dev/GroqAudioTranscription",
+    langSmithApiKey: import.meta.env.VITE_LANGSMITH_API_KEY, 
+    audioApiUrl: import.meta.env.VITE_AUDIO_API_URL,
     // audioApiUrl: "https://psct29qw0l.execute-api.us-east-1.amazonaws.com/dev",
 };
 
@@ -15,14 +15,6 @@ export const awsCognitoConfig = {
       userPoolId: 'us-east-1_Am52yaKth', // Reemplaza con tu User Pool ID
       userPoolClientId: '540k5l93t3kqud1up5flukg57e', // Reemplaza con tu App Client ID
       region: 'us-east-1', // Reemplaza con tu región, ej: 'us-east-1'
-      // Opcional: si tienes un dominio personalizado en Cognito
-      // oauth: {
-      //   domain: 'tu-dominio-cognito.auth.us-east-1.amazoncognito.com',
-      //   scope: ['email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
-      //   redirectSignIn: 'http://localhost:3000/', // o tu URL de despliegue
-      //   redirectSignOut: 'http://localhost:3000/logout/', // o tu URL de despliegue
-      //   responseType: 'code' // o 'token', 'code' es más seguro
-      // }
     }
   }
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import "./index.css";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.tsx';
 
-createRoot(document.getElementById("root")!).render(
-    <App />
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
